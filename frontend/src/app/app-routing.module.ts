ac00b00efcb3a190512153e91891752abf75df28
alvaroGD <agomezdelgado.guadalupe@alumnado.fundacionloyola.net>
2025-05-08 17:40:09 +0200

    Creacion de usuarios y primeros pasos

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -2,32 +2,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {InfoCentroComponent} from "./components/dashboard/info-centro/info-centro.component";
 import {InfoFormacionComponent} from "./components/dashboard/info-formacion/info-formacion.component";
+import {UsuariosComponent} from "./components/usuarios/usuarios.component";
 
 const routes: Routes = [
-  {
-    path: "info-centros",
-    component: InfoCentroComponent
-  },
-  {
-    path: "info-formaciones",
-    component: InfoFormacionComponent
-  },
-  {
-    path: "info-cursos",
-    component: InfoFormacionComponent
-  },
-  {
-    path: "info-educadores",
-    component: InfoFormacionComponent
-  },
-  {
-    path: "",
-    component: InfoCentroComponent
-  },
-  {
-    path: '**',
-    redirectTo: '/info-centros'
-  }
+  { path: '**', component: UsuariosComponent}
 ];
 
 @NgModule({
