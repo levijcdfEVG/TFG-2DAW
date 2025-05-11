8fb733211331bb88864492a21a5c8ed3974f1e34
alvaroGD <agomezdelgado.guadalupe@alumnado.fundacionloyola.net>
2025-05-11 18:51:06 +0200

    Controlador y modelo preparados, pendiente de detallar

--- backend/src/index.php
+++ backend/src/index.php
@@ -1,25 +1,26 @@
 <?php
-header("Access-Control-Allow-Origin: http://localhost:4200");
-header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
-header("Access-Control-Allow-Headers: Content-Type, Authorization");
+    header("Access-Control-Allow-Origin: http://localhost:4200");
+    header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+    header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
 
-    require_once 'config/config.php'; //Constantes config php
-    require_once MODELS.'conexion.php'; //Clase BBDD
+        require_once 'config/config.php'; //Constantes config php
+        require_once MODELS.'conexion.php'; //Clase BBDD
 
-//if(!isset($_GET["controller"])){$_GET["controller"] = DEFAULT_CONTROLLER;}
-//if(!isset($_GET["accion"])){$_GET["accion"] = DEFAULT_ACCION;}
+    //if(!isset($_GET["controller"])){$_GET["controller"] = DEFAULT_CONTROLLER;}
+    //if(!isset($_GET["accion"])){$_GET["accion"] = DEFAULT_ACCION;}
 
-$rutaControlador = CONTROLLERS.$_GET["controlador"].'.php'; // 'controller/cControlador.php'
+    $rutaControlador = CONTROLLERS.$_GET["controlador"].'.php'; // 'controller/cControlador.php'
 
-//if(!file_exists($rutaControlador)){$rutaControlador = CONTROLLERS.'c'.DEFAULT_CONTROLADOR.'.php';} // 'controller/cPais.php'
+    //if(!file_exists($rutaControlador)){$rutaControlador = CONTROLLERS.'c'.DEFAULT_CONTROLADOR.'.php';} // 'controller/cPais.php'
 
-require_once $rutaControlador;
+    require_once $rutaControlador;
 
-$nombreControlador = $_GET["controlador"]; //nombre de la clase controlador (Ejemplo: cPais)
-$controlador = new $nombreControlador(); //Instanciamos objeto de la clase controlador
+    $nombreControlador = $_GET["controlador"]; //nombre de la clase controlador (Ejemplo: cPais)
+    $controlador = new $nombreControlador(); //Instanciamos objeto de la clase controlador
 
-$dataToView["data"] = array();
-if(method_exists($controlador,$_GET["accion"])){
-    $dataToView["data"] = $controlador->{$_GET["accion"]}();
-}
+    $dataToView["data"] = array();
+    if(method_exists($controlador,$_GET["accion"])){
+        $dataToView["data"] = $controlador->{$_GET["accion"]}();
+    }
+?>
 
---
+++ backend/src/models/mUsuarios.php
@@ -0,0 +1,203 @@
+<?php
+
+class mUsuarios {
+
+    private $conexion;
+
+    public function conectar(){
+        $objetoBD = new bbdd(); //Conectamos a la base de datos. Creamos objeto $objetoBD
+        $this->conexion = $objetoBD->conexion; //Llamamos al metodo que realiza la conexion a la BBDD
+    }
+
+    public function listaCentros(){
+        $this->conectar(); //Llamo al metodo conectar de arriba
+
+        $sql = 'SELECT cf.*, l.nombre_localidad FROM centro_fundacion cf
+	        INNER JOIN localidad l ON cf.id_local = l.id'; //Escribimos la consulta
+        $stmt = $this->conexion->prepare($sql); // Preparamos la consulta
+        $stmt->execute(); // Ejecutamos la consulta
+
+        $resultados = $stmt->fetchAll(PDO::FETCH_ASSOC); // Obtenemos los resultados como un array asociativo
+
+        // Verificar si hay filas
+        if (empty($resultados)) {
+            return ['success' => false, 'message' => 'No se encontraron registros en la tabla.'];
+        }
+        else return ['success' => true, 'data' => $resultados]; // Retornar los datos si hay registros
+    }
+
+    public function insertIntoCentros($nombreCentro, $direccionCentro, $cpCentro, $localidadCentro, $telefonoCentro, $emailCentro) {
+        $this->conectar(); // Conectar a la base de datos
+        try {
+            //Verificar que la localidad existe
+            $sql = 'SELECT id FROM localidad WHERE nombre_localidad = :localidadCentro';
+            $stmt = $this->conexion->prepare($sql); // Preparamos la consulta
+            $stmt->execute([':localidadCentro' => $localidadCentro]); // Ejecutamos la consulta con el parámetro
+            $resultado = $stmt->fetch(PDO::FETCH_ASSOC); // Obtenemos el resultado como un array asociativo
+
+            if ($resultado) {
+                // Si existe la localidad, obtenemos su ID
+                $localidadId = $resultado['id'];
+            } else {
+                $provincia = substr($cpCentro, 0, 2); // Obtenemos la provincia del código postal
+                // Si no existe la localidad, hacemos un insert de la localidad
+                $sql = 'INSERT INTO localidad (nombre_localidad, provincia_id) VALUES (:localidadCentro, :provincia)';
+                $stmt = $this->conexion->prepare($sql); // Preparamos la consulta
+                $stmt->execute([':localidadCentro' => $localidadCentro, ':provincia' => $provincia]); //
+
+                // Obtenemos el ID de la localidad recién insertada
+                $localidadId = $this->conexion->lastInsertId();
+            }
+
+            $sql = 'INSERT INTO centro_fundacion (nombre_centro, direccion_centro, cp, correo_centro, telefono_centro, id_local)
+                VALUES (:nombreCentro, :direccionCentro, :cpCentro, :emailCentro, :telefonoCentro, :localidadId)';
+            $stmt = $this->conexion->prepare($sql);
+            $stmt->execute([
+                ':nombreCentro' => $nombreCentro,
+                ':direccionCentro' => $direccionCentro,
+                ':cpCentro' => $cpCentro,
+                ':emailCentro' => $emailCentro,
+                ':telefonoCentro' => $telefonoCentro,
+                ':localidadId' => $localidadId
+            ]);
+
+            // Si todo fue exitoso, devolver un mensaje de éxito
+            return ['success' => true, 'message' => 'Centro insertado correctamente'];
+        } catch (PDOException $e) {
+            // Manejar errores de la base de datos
+            return ['success' => false, 'message' => 'Error al insertar el centro: ' . $e->getMessage()];
+        }
+    }
+
+    public function modificarCentro($emailReferencia, $nombreCentro, $direccionCentro, $cpCentro, $localidadCentro, $telefonoCentro, $emailCentro) {
+        $this->conectar();
+
+        try{
+            $this->conexion->beginTransaction();
+
+            //Paso 1: borrar el centro con el email de referencia
+            $sql = "DELETE FROM centro_fundacion WHERE correo_centro = :emailReferencia";
+            $stmt = $this->conexion->prepare($sql);
+            $stmt->execute([':emailReferencia' => $emailReferencia]);
+
+            //Paso 2: verificar si la localidad existe
+            $sql = 'SELECT id FROM localidad WHERE nombre_localidad = :localidadCentro';
+            $stmt = $this->conexion->prepare($sql); // Preparamos la consulta
+            $stmt->execute([':localidadCentro' => $localidadCentro]); // Ejecutamos la consulta con el parámetro
+            $resultado = $stmt->fetch(PDO::FETCH_ASSOC); // Obtenemos el resultado como un array asociativo
+
+            if ($resultado) {
+                // Si existe la localidad, obtenemos su ID
+                $localidadId = $resultado['id'];
+            } else {
+                $provincia = substr($cpCentro, 0, 2); // Obtenemos la provincia del código postal
+                // Si no existe la localidad, hacemos un insert de la localidad
+                $sql = 'INSERT INTO localidad (nombre_localidad, provincia_id) VALUES (:localidadCentro, :provincia)';
+                $stmt = $this->conexion->prepare($sql); // Preparamos la consulta
+                $stmt->execute([':localidadCentro' => $localidadCentro, ':provincia' => $provincia]); //
+
+                // Obtenemos el ID de la localidad recién insertada
+                $localidadId = $this->conexion->lastInsertId();
+            }
+
+            //Paso 3: insertar el nuevo centro
+            $sql = "INSERT INTO centro_fundacion
+                    (nombre_centro, direccion_centro, cp, correo_centro, telefono_centro, id_local)
+                    VALUES (:nombreCentro, :direccionCentro, :cpCentro, :emailCentro, :telefonoCentro, :localidadId)";
+            $stmt = $this->conexion->prepare($sql);
+
+            // Ejecutar la consulta con los datos proporcionados
+            $stmt->execute([
+                ':nombreCentro' => $nombreCentro,
+                ':direccionCentro' => $direccionCentro,
+                ':cpCentro' => $cpCentro,
+                ':emailCentro' => $emailCentro,
+                ':telefonoCentro' => $telefonoCentro,
+                ':localidadId' => $localidadId
+            ]);
+            // Confirmar la transacción
+            $this->conexion->commit();
+            return ['success' => true, 'message' => 'Centro modificado con éxito'];
+        } catch (PDOException $e) {
+            // Si ocurre un error, hacer rollback
+            $this->conexion->rollBack();
+            return ['success' => false, 'message' => 'Error al modificar el centro: ' . $e->getMessage()];
+        }
+    }
+
+    public function eliminarCentro($emailReferencia) {
+        $this->conectar(); // Conectar a la base de datos
+        try {
+            // Paso 1: borrar el centro con el email de referencia
+            $sql = "DELETE FROM centro_fundacion WHERE correo_centro = :emailReferencia";
+            $stmt = $this->conexion->prepare($sql);
+            $stmt->execute([':emailReferencia' => $emailReferencia]);
+
+            // Si todo fue exitoso, devolver un mensaje de éxito
+            return ['success' => true, 'message' => 'Centro eliminado correctamente'];
+        } catch (PDOException $e) {
+            // Manejar errores de la base de datos
+            return ['success' => false, 'message' => 'Error al eliminar el centro: ' . $e->getMessage()];
+        }
+    }
+
+    public function validarLocalidad($nombreLocalidad, $idProvincia) {
+        $this->conectar();
+        $sql = "SELECT * FROM localidad WHERE nombre_localidad = :nombre_localidad AND provincia_id = :id_provincia";
+        $stmt = $this->conexion->prepare($sql);
+        $stmt->execute([':nombre_localidad' => $nombreLocalidad, ':id_provincia' => $idProvincia]);
+
+        if ($stmt->rowCount() > 0) {
+            return ['success' => true, 'message' => 'Localidad válida'];
+        } else {
+            return ['success' => false, 'message' => 'La localidad no se enccuentra en la provincia correspondiente al código postal'];
+        }
+    }
+
+    public function validarDatosCentro($nombreCentro, $direccionCentro, $cpCentro, $localidadCentro, $telefonoCentro, $emailCentro){
+        $this->conectar(); // Conectar a la base de datos
+        $sql = "SELECT id FROM localidad WHERE nombre_localidad = :nombre_localidad";
+        $stmt = $this->conexion->prepare($sql);
+        $stmt->execute([':nombre_localidad' => $localidadCentro]);
+        $resultado = $stmt->fetch(PDO::FETCH_ASSOC); // Obtenemos el resultado como un array asociativo
+        if ($resultado) {
+            // Si existe la localidad, obtenemos su ID
+            $localidadId = $resultado['id'];
+        } else {
+            return ['success' => false, 'message' => 'La localidad no existe'];
+        }
+        $sql = "SELECT * FROM centro_fundacion WHERE nombre_centro = :nombreCentro AND direccion_centro = :direccionCentro AND cp = :cpCentro AND id_local = :localidadId AND telefono_centro = :telefonoCentro AND correo_centro = :emailCentro";
+        $stmt = $this->conexion->prepare($sql);
+        $stmt->execute([
+            ':nombreCentro' => $nombreCentro,
+            ':direccionCentro' => $direccionCentro,
+            ':cpCentro' => $cpCentro,
+            ':localidadId' => $localidadId,
+            ':telefonoCentro' => $telefonoCentro,
+            ':emailCentro' => $emailCentro
+        ]);
+
+        if ($stmt->rowCount() > 0) {
+            return ['success' => false, 'message' => 'Los datos del centro no han cambiado'];
+        } else {
+            return ['success' => true, 'message' => 'Datos válidos'];
+        }
+    }
+
+    public function localidadExiste($nombreLocalidad) {
+        $this->conectar(); // Conectar a la base de datos
+        $sql = 'SELECT id FROM localidad WHERE nombre_localidad = :nombreLocalidad';
+        $stmt = $this->conexion->prepare($sql); // Preparamos la consulta
+        $stmt->execute([':nombreLocalidad' => $nombreLocalidad]); // Ejecutamos la consulta con el parámetro
+
+        $resultado = $stmt->fetch(PDO::FETCH_ASSOC); // Obtenemos el resultado como un array asociativo
+
+        if ($resultado) {
+            return ['success' => true, 'id' => $resultado['id']]; // Retornamos el ID de la localidad si existe
+        } else {
+            return ['success' => false, 'message' => 'La localidad no existe'];
+        }
+}
+}
+
+?>
